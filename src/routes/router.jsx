207e88c5d508454f6c91407ad96d67ce7d049b0b
MahidunNobi <mdmahidunnobi@gmail.com>
2024-10-20 10:21:19 +0600

    Production attempt-01

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,16 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Layout from "../layout/layout";
+import Layout from "../layout/Layout";
 
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-            index: true,
-            element: <App />
-        }
-      ]
-    },
-  ]);
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <App />,
+      },
+    ],
+  },
+]);
