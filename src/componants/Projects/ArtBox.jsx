9181d8fe65508f458576035ada8714f4e80f393a
MahidunNobi <mdmahidunnobi@gmail.com>
2024-10-20 10:33:46 +0600

    Production attempt-03

--- src/componants/Projects/ArtBox.jsx
+++ src/componants/Projects/ArtBox.jsx
@@ -1,11 +1,9 @@
-import artBoxImage from "../../assets/Projects/art-box.png";
-
 const ArtBox = () => {
   return (
     <div className="shadow-lg border flex flex-col md:flex-row p-4 rounded-lg gap-6 bg-gray-100">
       {/* Image */}
       <div className="max-w-96 rounded-lg overflow-hidden">
-        <img src={artBoxImage} alt="" />
+        <img src="https://ibb.co.com/9ppc0JZ" alt="" />
       </div>
       {/* Content */}
       <div className=" flex flex-col justify-between gap-3">
